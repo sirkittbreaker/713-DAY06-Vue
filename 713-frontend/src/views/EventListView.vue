4230c94bdb930c84984640821787e3e80347b752
sirkittbreaker <kittienan.kitkiki@gmail.com>
2025-03-08 11:41:16 +0700

    Task 3 using v-directive, v-if, v-for and ref object

--- 713-frontend/src/views/EventListView.vue
+++ 713-frontend/src/views/EventListView.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import EventCard from '@/components/EventCard.vue'
+const events = ref([
+  { id: 1, name: 'Event 1' },
+  { id: 2, name: 'Event 2' },
+  { id: 3, name: 'Event 3' },
+])
 </script>
 
 <template>
   <div>
-    <EventCard />
-    <EventCard />
-    <EventCard />
+    <!-- <div v-if="events.length === 0">No events</div>
+    <div v-else-if="events.length === 1">Only one event</div>
+    <div v-else>
+      <EventCard v-for="event in events" :key="event.id" />
+    </div> -->
+    <EventCard v-for="event in events" :key="event.id" />
   </div>
 </template>
