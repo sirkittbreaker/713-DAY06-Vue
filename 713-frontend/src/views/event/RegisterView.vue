70cb9683e9175a1c2fb9f88d9103c2af92708911
sirkittbreaker <kittienan.kitkiki@gmail.com>
2025-03-09 00:24:32 +0700

    Task 13 using Pinia for state management

--- 713-frontend/src/views/event/RegisterView.vue
+++ 713-frontend/src/views/event/RegisterView.vue
@@ -2,15 +2,21 @@
 import { toRefs } from 'vue'
 import type { Event } from '@/types'
 import { useRouter } from 'vue-router'
+import { useMessageStore } from '@/stores/message'
 
 const props = defineProps<{
   event: Event
   id: string
 }>()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+const store = useMessageStore()
 const { event } = toRefs(props)
 const router = useRouter()
 const register = () => {
+  store.updateMessage('You are successfully registered for ' + props.event.title)
+  setTimeout(() => {
+    store.resetMessage()
+  }, 3000)
   router.push({ name: 'event-detail-view' })
 }
 </script>
