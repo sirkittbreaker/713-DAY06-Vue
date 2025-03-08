25c231fe1eddbd30a70a797ef937b6a990b839ad
sirkittbreaker <kittienan.kitkiki@gmail.com>
2025-03-09 00:53:02 +0700

    Task 15 using Pinia to manage focused data

--- 713-frontend/src/router/index.ts
+++ 713-frontend/src/router/index.ts
@@ -7,6 +7,8 @@ import EventLayoutView from '@/views/event/LayoutView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
 import NetworkErrorView from '@/views/NetworkErrorView.vue'
 import nProgress from 'nprogress'
+import { useEventStore } from '@/stores/event'
+import EventService from '@/services/EventService'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -50,6 +52,26 @@ const router = createRouter({
       name: 'event-layout-view',
       component: EventLayoutView,
       props: true,
+      beforeEnter: (to) => {
+        const id = parseInt(to.params.id as string)
+        const eventStore = useEventStore()
+        return EventService.getEvent(id)
+          .then((response) => {
+            // need to setup the data for the event
+            eventStore.setEvent(response.data)
+          })
+          .catch((error) => {
+            if (error.response && error.response.status === 404) {
+              return {
+                name: '404-resource-view',
+                params: { resource: 'event' },
+              }
+            } else {
+              return { name: 'network-error-view' }
+            }
+          })
+      },
+
       children: [
         {
           path: '',
--- 713-frontend/src/views/event/LayoutView.vue
+++ 713-frontend/src/views/event/LayoutView.vue
@@ -1,38 +1,25 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import type { Event } from '@/types'
-import EventService from '@/services/EventService'
-import { useRouter } from 'vue-router'
-
-const event = ref<Event>()
+import { useEventStore } from '@/stores/event'
+import { storeToRefs } from 'pinia'
+const store = useEventStore()
+const { event } = storeToRefs(store)
 const props = defineProps<{ id: string }>()
-const id = Number(props.id)
-const router = useRouter()
-
-EventService.getEvent(id)
-  .then((response) => {
-    event.value = response.data
-  })
-  .catch((error) => {
-    if (error.response && error.response.status === 404) {
-      router.push({ name: '404-resource-view', params: { resource: 'event' } })
-    } else {
-      router.push({ name: 'network-error-view' })
-    }
-    // console.error('There was an error!', error)
-    // router.push({ name: '404-resource-view', params: { resource: 'event' } })
-  })
 </script>
+
 <template>
   <div>
     <div v-if="event">
       <h1>{{ event.title }}</h1>
       <nav>
-        <router-link :to="{ name: 'event-detail-view', params: { id } }">Details</router-link>
+        <router-link :to="{ name: 'event-detail-view', params: { id: props.id } }"
+          >Details</router-link
+        >
         |
-        <router-link :to="{ name: 'event-register-view', params: { id } }">Register</router-link>
+        <router-link :to="{ name: 'event-register-view', params: { id: props.id } }"
+          >Register</router-link
+        >
         |
-        <router-link :to="{ name: 'event-edit-view', params: { id } }">Edit</router-link>
+        <router-link :to="{ name: 'event-edit-view', params: { id: props.id } }">Edit</router-link>
       </nav>
       <router-view :event="event"></router-view>
     </div>
