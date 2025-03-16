ba05d6dafc3d273d93a60e57a27fde35ede81de5
sirkittbreaker <kittienan.kitkiki@gmail.com>
2025-03-16 23:52:04 +0700

    lab07 task 10 Adding a Login Menu

--- 713-frontend/src/shims.vue.d.ts
+++ 713-frontend/src/shims.vue.d.ts
@@ -3,3 +3,8 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+declare module '@jamescoyle/vue-icon' {
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
--- 713-frontend/src/stores/auth.ts
+++ 713-frontend/src/stores/auth.ts
@@ -1,11 +1,19 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
 import apiClient from '@/services/AxiosClient'
+import type { Organizer } from '@/types'
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     token: null as string | null,
+    user: null as Organizer | null,
   }),
+  getters: {
+    currentUserName(): string {
+      return this.user?.name || ''
+    },
+  },
+
   actions: {
     async login(email: string, password: string) {
       const response = await apiClient.post('/api/v1/auth/authenticate', {
@@ -14,7 +22,18 @@ export const useAuthStore = defineStore('auth', {
       })
       this.token = response.data.access_token
       localStorage.setItem('token', this.token as string)
+      const meResponse = await apiClient.get('/api/v1/auth/me')
+      this.user = meResponse.data.user
+      localStorage.setItem('user', JSON.stringify(this.user))
+
       return response
+    },
+    logout() {
+      console.log('logout')
+      this.token = null
+      this.user = null
+      localStorage.removeItem('access_token')
+      localStorage.removeItem('user')
     },
   },
 })
